ba6e2ee51fb68db6315ae9b6057e13ea935c863a
Alexandra Stricklin <stricklin927@gmail.com>
2020-10-02 23:28:12 -0400

    edited style.css

--- script.js
+++ script.js
@@ -107,13 +107,17 @@ function generatePassword() {
     return ps;
 }
 
-// Add event listener to generate button
+// Add event listener to generate button ?????????????????????????
 generateBtn.addEventListener("click", writePassword);
+
+
 
 // Generates new password into textbox
 function UserInput(ps) {
   document.getElementById("password").textContent = ps;
 }
+
+
 
 // Copy to clipboard button
 var copy = document.querySelector("#copy");
@@ -122,6 +126,6 @@ copy.addEventListener("click", function() {
 });
 function copyPassword() {
   document.getElementById("password").select();
-  document.execCommand("Copy");
+  document.execCommand("copy");
   alert("Copied to Clipboard");
 }
